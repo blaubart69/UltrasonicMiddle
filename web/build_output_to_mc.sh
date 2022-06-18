f9186a5c6132d027c3d7900ed96dfb1518d6365a
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2022-06-18 11:18:48 +0200

    create gzip files in buildscript

--- web/build_output_to_mc.sh
+++ web/build_output_to_mc.sh
@@ -1,1 +1,3 @@
 ng build -c production --output-hashing none --output-path ../mc/data
+gzip -k ../mc/data/*.html ../mc/data/*.js ../mc/data/*.css
+rm ../mc/data/*.html ../mc/data/*.js ../mc/data/*.css
